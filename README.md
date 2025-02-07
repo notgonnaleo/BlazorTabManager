aac314d2c33a3f9fa9c2e164a52a1972ed15cfb0
Leonardo <lbruni10@gmail.com>
2025-02-06 21:44:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Copying salesforce UI/UX but for a blazor app, doing in the worst way possible maybe?
+I'm not sure if this is a good idea, but I'm going to try it anyway.
---
+++ Util/TabDataService.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorTabManagerApp.Util
+{
+    public class TabDataService
+    {
+        public Dictionary<Type, object> TabData { get; set; } = new Dictionary<Type, object>();
+        public T GetOrCreateTabData<T>(Type tab)
+        {
+            if (TabData.ContainsKey(tab))
+            {
+                return (T)TabData[tab];
+            }
+            else
+            {
+                var newData = Activator.CreateInstance<T>();
+                TabData[tab] = newData;
+                return newData;
+            }
+        }
+    }
+}
